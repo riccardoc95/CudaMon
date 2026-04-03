a08be8f1663c27042ae6f5ef10991c2d5a2bd6ef
riccardo <riccardo.ceccaroni.95@gmail.com>
2026-04-03 13:42:17 +0200

    drop rlang

--- R/nvml.R
+++ R/nvml.R
@@ -6,46 +6,42 @@
 #' Initialise NVML
 #' @export
 nvml_init <- function() {
-  status <- .Call(nvml_init_c)
-  nvml_check_status(status, call = rlang::caller_env())
+  status <- .Call("nvml_init_c", PACKAGE = "CudaMon")
+  nvml_check_status(status)
   invisible(TRUE)
 }
 
 #' Shut down NVML
 #' @export
 nvml_shutdown <- function() {
-  status <- .Call(nvml_shutdown_c)
-  nvml_check_status(status, call = rlang::caller_env())
+  status <- .Call("nvml_shutdown_c", PACKAGE = "CudaMon")
+  nvml_check_status(status)
   invisible(TRUE)
 }
 
-#' Number of NVML‑visible devices
+#' Number of NVML-visible devices
 #' @export
 nvml_device_count <- function() {
-  cnt <- .Call(nvml_device_count_c)
+  cnt <- .Call("nvml_device_count_c", PACKAGE = "CudaMon")
   if (cnt < 0L) {
-    nvml_check_status(cnt, call = rlang::caller_env())
+    nvml_check_status(cnt)
   }
   as.integer(cnt)
 }
 
 #' Get metrics for a device
 #'
-#' @param device_index Integer, 0‑based GPU index
+#' @param device_index Integer, 0-based GPU index
 #' @export
 nvml_get_metrics <- function(device_index) {
   if (!is.numeric(device_index) || length(device_index) != 1L) {
-    rlang::abort(
-      "device_index must be a single numeric value",
-      class = "nvml_error",
-      .call = rlang::caller_env()
-    )
+    stop("device_index must be a single numeric value", call. = FALSE)
   }
   idx <- as.integer(device_index)
 
-  res <- .Call(nvml_get_metrics_c, idx)
+  res <- .Call("nvml_get_metrics_c", idx, PACKAGE = "CudaMon")
   if (is.integer(res) && length(res) == 1L && res < 0L) {
-    nvml_check_status(res, call = rlang::caller_env())
+    nvml_check_status(res)
   }
   res
 }
--- R/utils.R
+++ R/utils.R
@@ -1,15 +1,10 @@
-#' Convert an NVML error code to a tidy rlang condition
+#' Convert an NVML error code to an R error
 #'
 #' @param code Integer NVML return code (0 == success)
-#' @param call The call environment to attach (default caller_env())
-#' @return Invisible TRUE on success; otherwise aborts with class "nvml_error"
-nvml_check_status <- function(code, call = rlang::caller_env()) {
+#' @return Invisible TRUE on success; otherwise stops with an error
+nvml_check_status <- function(code) {
   if (!is.numeric(code) && !is.integer(code)) {
-    rlang::abort(
-      "Internal error: NVML status code is not numeric",
-      class = "nvml_error",
-      .call = call
-    )
+    stop("Internal error: NVML status code is not numeric", call. = FALSE)
   }
 
   code <- as.integer(code)
@@ -18,14 +13,6 @@ nvml_check_status <- function(code, call = rlang::caller_env()) {
     return(invisible(TRUE))
   }
 
-  msg <- .Call(nvml_error_string_c, code)
-  rlang::abort(
-    message = c(
-      paste0("NVML error: ", msg),
-      "i" = paste0("Error code: ", code)
-    ),
-    class   = "nvml_error",
-    nvml_code = code,
-    .call   = call
-  )
+  msg <- .Call("nvml_error_string_c", code, PACKAGE = "CudaMon")
+  stop(sprintf("NVML error: %s (code: %d)", msg, code), call. = FALSE)
 }
