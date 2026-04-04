b9698c57870b6e2ab3f0765fe5ecd776048d3fe9
riccardo <riccardo.ceccaroni.95@gmail.com>
2026-04-04 09:55:08 +0200

    cleanup r api

--- R/nvml.R
+++ R/nvml.R
@@ -1,23 +1,6 @@
 # Copyright (c) 2025 Mohammad Amin Zadenoori
 # Copyright (c) 2025 Gabriele Sales
 #
-# This software is licensed under the Artistic License 2.0.
-
-#' Initialise NVML
-#' @export
-nvml_init <- function() {
-  status <- .Call("nvml_init_c", PACKAGE = "CudaMon")
-  nvml_check_status(status)
-  invisible(TRUE)
-}
-
-#' Shut down NVML
-#' @export
-nvml_shutdown <- function() {
-  status <- .Call("nvml_shutdown_c", PACKAGE = "CudaMon")
-  nvml_check_status(status)
-  invisible(TRUE)
-}
 
 #' Number of NVML-visible devices
 #' @export
@@ -32,6 +15,8 @@ nvml_device_count <- function() {
 #' Get metrics for a device
 #'
 #' @param device_index Integer, 0-based GPU index
+#' @return A named list with utilization percentages, temperature in Celsius,
+#'   power draw in milliwatts, and memory usage in bytes
 #' @export
 nvml_get_metrics <- function(device_index) {
   if (!is.numeric(device_index) || length(device_index) != 1L) {
@@ -39,17 +24,11 @@ nvml_get_metrics <- function(device_index) {
   }
   idx <- as.integer(device_index)
 
-  res <- .Call("nvml_get_metrics_c", idx, PACKAGE = "CudaMon")
-  if (is.integer(res) && length(res) == 1L && res < 0L) {
-    nvml_check_status(res)
-  }
-  res
+  nvml_as_metrics(.Call("nvml_get_metrics_c", idx, PACKAGE = "CudaMon"))
 }
 
 #' Demo function showing typical usage
 #' @export
-demo <- function() {
-  nvml_init()
-  on.exit(nvml_shutdown(), add = TRUE)
+nvml_demo <- function() {
   nvml_get_metrics(0)
 }
