27270f9c8dfe29049382b83537e2288588c71df8
Riccardo Ceccaroni <riccardo.ceccaroni.95@gmail.com>
2026-05-19 15:37:22 +0200

    restore viz.R

--- R/viz.R
+++ R/viz.R
@@ -94,10 +94,10 @@ cm_plot_usage <- function(x, tz = "UTC", device_index = NULL) {
   plot_df <- cm_vizdf(x, tz = tz, device_index = device_index)
   p <- ggplot2::ggplot(
     plot_df,
-    ggplot2::aes(x = plot_df$tm, y = plot_df$value)
+    ggplot2::aes(x = tm, y = value)
   ) +
     ggplot2::geom_point() +
-    ggplot2::facet_grid(ggplot2::vars(plot_df$type), scales = "free") +
+    ggplot2::facet_grid(ggplot2::vars(type), scales = "free") +
     ggplot2::scale_x_datetime(
       timezone = tz,
       date_labels = "%H:%M:%S"
@@ -113,7 +113,7 @@ cm_plot_usage <- function(x, tz = "UTC", device_index = NULL) {
     p <- p +
       ggplot2::geom_vline(
         data = events_df,
-        ggplot2::aes(xintercept = events_df$tm),
+        ggplot2::aes(xintercept = tm),
         inherit.aes = FALSE,
         linetype = "dashed",
         color = "firebrick"
@@ -121,9 +121,9 @@ cm_plot_usage <- function(x, tz = "UTC", device_index = NULL) {
       ggplot2::geom_text(
         data = events_df,
         ggplot2::aes(
-          x = events_df$tm,
-          y = events_df$label_y,
-          label = events_df$step
+          x = tm,
+          y = label_y,
+          label = step
         ),
         inherit.aes = FALSE,
         angle = 90,
