0f55c8847c74f440cec89c8b020e8a1997a1d722
Riccardo Ceccaroni <riccardo.ceccaroni.95@gmail.com>
2026-05-21 09:02:08 +0200

    code refactoring

--- R/cm_session.R
+++ R/cm_session.R
@@ -44,59 +44,69 @@
 #' session
 #' @export
 CudaMonSession <- function(
-  device_metrics = data.frame(),
-  compute_processes = data.frame(),
-  events = data.frame(),
-  paths = list(),
-  metadata = list()
+    device_metrics = data.frame(),
+    compute_processes = data.frame(),
+    events = data.frame(),
+    paths = list(),
+    metadata = list()
 ) {
-  structure(
-    list(
-      device_metrics = device_metrics,
-      compute_processes = compute_processes,
-      events = events,
-      paths = paths,
-      metadata = metadata
-    ),
-    class = "CudaMonSession"
-  )
+    structure(
+        list(
+            device_metrics = device_metrics,
+            compute_processes = compute_processes,
+            events = events,
+            paths = paths,
+            metadata = metadata
+        ),
+        class = "CudaMonSession"
+    )
 }
 
 #' @export
 print.CudaMonSession <- function(x, ...) {
-  cat("CudaMonSession\n")
-  cat("  Device samples: ", nrow(x$device_metrics), "\n", sep = "")
-  cat("  GPU process samples: ", nrow(x$compute_processes), "\n", sep = "")
-  cat("  Event markers: ", nrow(x$events), "\n", sep = "")
+    cat("CudaMonSession\n")
+    cat("  Device samples: ", nrow(x$device_metrics), "\n", sep = "")
+    cat("  GPU process samples: ", nrow(x$compute_processes), "\n", sep = "")
+    cat("  Event markers: ", nrow(x$events), "\n", sep = "")
 
-  if (length(x$paths) > 0L) {
-    if (!is.null(x$paths$device_metrics)) {
-      cat("  Device metrics path: ", x$paths$device_metrics, "\n", sep = "")
+    if (length(x$paths) > 0L) {
+        if (!is.null(x$paths$device_metrics)) {
+            cat(
+                "  Device metrics path: ",
+                x$paths$device_metrics,
+                "\n",
+                sep = ""
+            )
+        }
+        if (!is.null(x$paths$compute_processes)) {
+            cat(
+                "  Compute processes path: ",
+                x$paths$compute_processes,
+                "\n",
+                sep = ""
+            )
+        }
+        if (!is.null(x$paths$events)) {
+            cat("  Events path: ", x$paths$events, "\n", sep = "")
+        }
+        if (!is.null(x$paths$log)) {
+            cat("  Sampler log path: ", x$paths$log, "\n", sep = "")
+        }
     }
-    if (!is.null(x$paths$compute_processes)) {
-      cat("  Compute processes path: ", x$paths$compute_processes, "\n", sep = "")
-    }
-    if (!is.null(x$paths$events)) {
-      cat("  Events path: ", x$paths$events, "\n", sep = "")
-    }
-    if (!is.null(x$paths$log)) {
-      cat("  Sampler log path: ", x$paths$log, "\n", sep = "")
-    }
-  }
 
-  if (length(x$metadata) > 0L) {
-    if (!is.null(x$metadata$root_pid)) {
-      cat("  Root PID: ", x$metadata$root_pid, "\n", sep = "")
+    if (length(x$metadata) > 0L) {
+        if (!is.null(x$metadata$root_pid)) {
+            cat("  Root PID: ", x$metadata$root_pid, "\n", sep = "")
+        }
+        if (!is.null(x$metadata$include_descendants)) {
+            cat(
+                "  Include descendants: ",
+                x$metadata$include_descendants,
+                "\n",
+                sep = ""
+            )
+        }
     }
-    if (!is.null(x$metadata$include_descendants)) {
-      cat(
-        "  Include descendants: ",
-        x$metadata$include_descendants,
-        "\n",
-        sep = ""
-      )
-    }
-  }
 
-  invisible(x)
+    invisible(x)
 }
--- R/process_pids.R
+++ R/process_pids.R
@@ -5,24 +5,24 @@
 #' @return Integer vector of process identifiers.
 #' @noRd
 process_pids <- function(pid, include_descendants = TRUE) {
-  pid <- as.integer(pid)
-  if (!include_descendants) {
-    return(pid)
-  }
+    pid <- as.integer(pid)
+    if (!include_descendants) {
+        return(pid)
+    }
 
-  root <- tryCatch(
-    ps::ps_handle(pid),
-    error = function(...) NULL
-  )
-  if (is.null(root)) {
-    return(pid)
-  }
+    root <- tryCatch(
+        ps::ps_handle(pid),
+        error = function(...) NULL
+    )
+    if (is.null(root)) {
+        return(pid)
+    }
 
-  children <- tryCatch(
-    ps::ps_children(root, recursive = TRUE),
-    error = function(...) list()
-  )
-  child_pids <- vapply(children, ps::ps_pid, integer(1))
+    children <- tryCatch(
+        ps::ps_children(root, recursive = TRUE),
+        error = function(...) list()
+    )
+    child_pids <- vapply(children, ps::ps_pid, integer(1))
 
-  as.integer(c(pid, child_pids))
+    as.integer(c(pid, child_pids))
 }
